bd0eca2637c29edda9a525cabfce702ddae24729
Val Ovinnikov <valovinnikov@Vals-MacBook-Pro-3.local>
2026-07-07 10:37:53 +0100

    chore: centralize tailwind theme config

--- apps/web/src/config/fonts.ts
+++ apps/web/src/config/fonts.ts
@@ -3,18 +3,18 @@ import { JetBrains_Mono, Newsreader, Space_Grotesk } from 'next/font/google';
 export const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
   weight: ['400', '500', '700'],
-  variable: '--font-display',
+  variable: '--font-display-family',
 });
 
 export const newsreader = Newsreader({
   subsets: ['latin'],
   weight: ['400', '500'],
   style: ['normal', 'italic'],
-  variable: '--font-body',
+  variable: '--font-body-family',
 });
 
 export const jetbrainsMono = JetBrains_Mono({
   subsets: ['latin'],
   weight: ['400', '500'],
-  variable: '--font-mono',
+  variable: '--font-mono-family',
 });
