1a71fa2d563298cee8b866289b8f072930a33c06
Val Ovinnikov <val.ovinnikov@icloud.com>
2026-09-18 14:18:46 +0100

    test(config): assert the profile hero on the home and landing pages

--- packages/config/src/types/page-module.test.ts
+++ packages/config/src/types/page-module.test.ts
@@ -14,6 +14,7 @@ describe('page module type unions', () => {
     expectTypeOf<TPageHomeType>().toEqualTypeOf<
       | 'module_heroBlog'
       | 'module_heroStatement'
+      | 'module_heroProfile'
       | 'module_content'
       | 'module_cta'
       | 'module_newsletter'
@@ -33,6 +34,7 @@ describe('page module type unions', () => {
     expectTypeOf<TPageLandingType>().toEqualTypeOf<
       | 'module_heroBlog'
       | 'module_heroStatement'
+      | 'module_heroProfile'
       | 'module_content'
       | 'module_cta'
       | 'module_postLatest'
