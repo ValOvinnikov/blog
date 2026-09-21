04e129104ff785d6902af7a31c8cd228445554df
Val Ovinnikov <val.ovinnikov@icloud.com>
2026-09-21 11:13:58 +0100

    chore(config): regenerate Sanity types for testimonial schemas

--- packages/config/src/sanity/generated/types.ts
+++ packages/config/src/sanity/generated/types.ts
@@ -139,6 +139,39 @@ export type RichText = Array<
     } & Aside)
 >;
 
+export type Block_testimonialReference = {
+  _ref: string;
+  _type: 'reference';
+  _weak?: boolean;
+  [internalGroqTypeReferenceTo]?: 'block_testimonial';
+};
+
+export type Module_testimonial = {
+  _id: string;
+  _type: 'module_testimonial';
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  title?: string;
+  brandVariant?: 'PRIMARY' | 'SECONDARY';
+  headingBlock?: HeadingBlock;
+  testimonials?: Array<
+    {
+      _key: string;
+    } & Block_testimonialReference
+  >;
+  ctaButtons?: Array<
+    {
+      _key: string;
+    } & CtaButton
+  >;
+  showImages?: boolean;
+  displayMode?: 'GRID' | 'CAROUSEL';
+  contentAlignment?: 'LEFT' | 'CENTER' | 'RIGHT';
+  cardAlignment?: 'LEFT' | 'CENTER';
+  layout?: Layout;
+};
+
 export type Block_featureReference = {
   _ref: string;
   _type: 'reference';
@@ -526,6 +559,19 @@ export type Settings_site = {
   brand?: Brand;
 };
 
+export type Block_testimonial = {
+  _id: string;
+  _type: 'block_testimonial';
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  quote?: string;
+  name?: string;
+  role?: string;
+  photo?: ImageWithAlt;
+  link?: LinkReference;
+};
+
 export type Block_feature = {
   _id: string;
   _type: 'block_feature';
@@ -908,6 +954,13 @@ export type Module_featureListReference = {
   [internalGroqTypeReferenceTo]?: 'module_featureList';
 };
 
+export type Module_testimonialReference = {
+  _ref: string;
+  _type: 'reference';
+  _weak?: boolean;
+  [internalGroqTypeReferenceTo]?: 'module_testimonial';
+};
+
 export type Page_landing = {
   _id: string;
   _type: 'page_landing';
@@ -943,6 +996,9 @@ export type Page_landing = {
     | ({
         _key: string;
       } & Module_featureListReference)
+    | ({
+        _key: string;
+      } & Module_testimonialReference)
   >;
   seo?: Seo;
 };
@@ -981,6 +1037,9 @@ export type Page_home = {
     | ({
         _key: string;
       } & Module_featureListReference)
+    | ({
+        _key: string;
+      } & Module_testimonialReference)
   >;
   seo?: Seo;
 };
@@ -1260,6 +1319,8 @@ export type AllSanitySchemaTypes =
   | ImageWithAlt
   | Module_content
   | RichText
+  | Block_testimonialReference
+  | Module_testimonial
   | Block_featureReference
   | Module_featureList
   | Module_postRelated
@@ -1293,6 +1354,7 @@ export type AllSanitySchemaTypes =
   | Settings_footer
   | Settings_navigation
   | Settings_site
+  | Block_testimonial
   | Block_feature
   | Blog_author
   | Page_homeReference
@@ -1321,6 +1383,7 @@ export type AllSanitySchemaTypes =
   | Module_heroProfileReference
   | Module_contentReference
   | Module_featureListReference
+  | Module_testimonialReference
   | Page_landing
   | Page_home
   | Blog_authorReference
