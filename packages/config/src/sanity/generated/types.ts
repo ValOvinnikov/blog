29f9b933cc0666657aca3e05353e956c512b9336
Val Ovinnikov <val.ovinnikov@icloud.com>
2026-09-23 19:57:38 +0100

    chore(config): regenerate Sanity types for inline logo items

--- packages/config/src/sanity/generated/types.ts
+++ packages/config/src/sanity/generated/types.ts
@@ -171,13 +171,6 @@ export type Module_testimonial = {
   layout?: Layout;
 };
 
-export type Block_logoReference = {
-  _ref: string;
-  _type: 'reference';
-  _weak?: boolean;
-  [internalGroqTypeReferenceTo]?: 'block_logo';
-};
-
 export type Module_logoWall = {
   _id: string;
   _type: 'module_logoWall';
@@ -190,7 +183,7 @@ export type Module_logoWall = {
   logos?: Array<
     {
       _key: string;
-    } & Block_logoReference
+    } & LogoItem
   >;
   ctaButtons?: Array<
     {
@@ -468,6 +461,35 @@ export type SocialProfile = {
     | 'RSS';
 };
 
+export type LogoItem = {
+  _type: 'logoItem';
+  name?: string;
+  image?: {
+    asset?: SanityImageAssetReference;
+    media?: unknown;
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    _type: 'image';
+  };
+  link?: LinkReference;
+};
+
+export type SanityImageCrop = {
+  _type: 'sanity.imageCrop';
+  top?: number;
+  bottom?: number;
+  left?: number;
+  right?: number;
+};
+
+export type SanityImageHotspot = {
+  _type: 'sanity.imageHotspot';
+  x?: number;
+  y?: number;
+  height?: number;
+  width?: number;
+};
+
 export type LinkRef = {
   _type: 'linkRef';
   link?: LinkReference;
@@ -599,17 +621,6 @@ export type Block_testimonial = {
   name?: string;
   quote?: ListedText;
   role?: string;
-  image?: ImageWithAlt;
-  link?: LinkReference;
-};
-
-export type Block_logo = {
-  _id: string;
-  _type: 'block_logo';
-  _createdAt: string;
-  _updatedAt: string;
-  _rev: string;
-  title?: string;
   image?: ImageWithAlt;
   link?: LinkReference;
 };
@@ -1236,22 +1247,6 @@ export type Blog_topic = {
   description?: string;
 };
 
-export type SanityImageCrop = {
-  _type: 'sanity.imageCrop';
-  top?: number;
-  bottom?: number;
-  left?: number;
-  right?: number;
-};
-
-export type SanityImageHotspot = {
-  _type: 'sanity.imageHotspot';
-  x?: number;
-  y?: number;
-  height?: number;
-  width?: number;
-};
-
 export type MediaTag = {
   _id: string;
   _type: 'media.tag';
@@ -1377,7 +1372,6 @@ export type AllSanitySchemaTypes =
   | ArticleText
   | Block_testimonialReference
   | Module_testimonial
-  | Block_logoReference
   | Module_logoWall
   | Block_featureReference
   | Module_featureList
@@ -1401,6 +1395,9 @@ export type AllSanitySchemaTypes =
   | CtaSecondaryButton
   | CtaButton
   | SocialProfile
+  | LogoItem
+  | SanityImageCrop
+  | SanityImageHotspot
   | LinkRef
   | Aside
   | BodyImage
@@ -1413,7 +1410,6 @@ export type AllSanitySchemaTypes =
   | Settings_navigation
   | Settings_site
   | Block_testimonial
-  | Block_logo
   | Block_feature
   | Blog_author
   | Page_homeReference
@@ -1453,8 +1449,6 @@ export type AllSanitySchemaTypes =
   | Module_postRelatedReference
   | Page_post
   | Blog_topic
-  | SanityImageCrop
-  | SanityImageHotspot
   | MediaTag
   | Code
   | SanityImagePaletteSwatch
